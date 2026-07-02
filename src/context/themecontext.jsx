9655a15371b5447de0e4d33baf4585d54c51b275
web-dev-coders <wardamanzoor5@gmail.in>
2026-07-03 01:58:50 +0500

    Update ThemeContext implementation

--- src/context/themecontext.jsx
+++ src/context/themecontext.jsx
@@ -1,28 +1,26 @@
 import { createContext, useState, useEffect } from "react";
 
-// 1. Context banaya
 export const ThemeContext = createContext();
 
-// 2. Provider Component jo saare data ko handle karega
+
 export function ThemeProvider({ children }) {
     
-    // Local storage se purani selected theme check karte hain
+   
     const [theme, setTheme] = useState(() => {
         return localStorage.getItem("app-theme") || "light";
     });
 
-    // Jab bhi theme state badle, usay browser ki memory mein update karo
+   
     useEffect(() => {
         localStorage.setItem("app-theme", theme);
     }, [theme]);
 
-    // Theme badalne ka simple switch logic
     const toggleTheme = () => {
         setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     };
 
     return (
-        // Value ke zariye hum poori app ko data supply kar rahe hain
+        
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             {children}
         </ThemeContext.Provider>
